2aba0d6659e303a31f520dccbb03e482628698fc
luk3mn <lucasrenan2015@gmail.com>
2021-12-06 10:36:10 -0200

    correção do styled-components

--- App.js
+++ App.js
@@ -1,24 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View } from 'react-native';
+import styled from 'styled-components/native';
 
 const Page = styled.SafeAreaView`
-  flex: 1;
-  align-items: center;
+`;
+
+const ImageArea = styled.SafeAreaView`
 `;
 
 const TextArea = styled.Text`
-  font-size: 25px;
 `;
 
 const InputArea = styled.TextInput`
-  width: 90%;
-  height: 50px;
-  font-size: 18px;
-  background-color: #EEE;
-  margin-top: 20px;
-  border-radius: 10px;
-  padding: 10px;
+`;
+
+const ResultArea = styled.SafeAreaView`
 `;
 
 //cadeia de caracteres
@@ -26,6 +22,7 @@ let charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
 
 export default function app(){
   //criação das states
+  const [tamPass, setTamPass] = useState('');
   
   // função para a geração da senha
   function geraPsw(){
